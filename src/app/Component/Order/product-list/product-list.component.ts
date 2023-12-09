8b1b806e4cc4c66fd2fd7f73352c6305c41058e9
AliAamer04 <73921348+AliAamer04@users.noreply.github.com>
2023-12-09 10:59:43 +0100

    my-angularApp#

--- src/app/Component/Order/product-list/product-list.component.ts
+++ src/app/Component/Order/product-list/product-list.component.ts
@@ -19,12 +19,12 @@ export class ProductListComponent implements OnInit {
       { id: 3, name: 'Mobile' }
     ]
     this.prdList = [
-      { id: 100, name: 'lenovoThinkpad laptop', price: 100, quantity: 1, imgURL: 'https://picsum.photos/350/200', categoryID: 1 },
-      { id: 200, name: 'Apple Macbook laptop', price: 200, quantity: 5, imgURL: 'https://picsum.photos/350/200', categoryID: 1 },
-      { id: 300, name: 'lenovo tab 2', price: 300, quantity: 10, imgURL: 'https://picsum.photos/350/200', categoryID: 2 },
-      { id: 400, name: 'samsung tab', price: 400, quantity: 0, imgURL: 'https://picsum.photos/350/200', categoryID: 2 },
-      { id: 500, name: 'Samsungnote laptop', price: 500, quantity: 3, imgURL: 'https://picsum.photos/350/200', categoryID: 3 },
-      { id: 600, name: 'Apple Bookair laptop', price: 600, quantity: 1, imgURL: 'https://picsum.photos/350/200', categoryID: 3 }
+      { id: 100, name: 'lenovoThinkpad laptop', price: 1000000, quantity: 1, imgURL: 'https://picsum.photos/350/200', categoryID: 1 },
+      { id: 200, name: 'Apple Macbook laptop', price: 2077, quantity: 5, imgURL: 'https://picsum.photos/350/200', categoryID: 1 },
+      { id: 300, name: 'lenovo tab 2', price: 3030430, quantity: 10, imgURL: 'https://picsum.photos/350/200', categoryID: 2 },
+      { id: 400, name: 'samsung tab', price: 40022, quantity: 0, imgURL: 'https://picsum.photos/350/200', categoryID: 2 },
+      { id: 500, name: 'Samsungnote laptop', price: 50992, quantity: 3, imgURL: 'https://picsum.photos/350/200', categoryID: 3 },
+      { id: 600, name: 'Apple Bookair laptop', price: 60099, quantity: 1, imgURL: 'https://picsum.photos/350/200', categoryID: 3 }
     ]
   }
 
